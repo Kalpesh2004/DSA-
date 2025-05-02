2758358c087ca67a4c7cd2ef567196706a5c93bf
Kalpesh2004 <113294961+Kalpesh2004@users.noreply.github.com>
2025-05-02 22:41:39 +0530

    Add files via upload

---
+++ String/SearchChar.java
@@ -0,0 +1,29 @@
+public class SearchChar {
+    public static void main(String[] args) 
+    {
+           String name = "kalpesh";
+           char target = 's';
+
+           System.out.println(search(name,target));
+
+    }
+    static boolean search(String str,char target)
+    {
+        if(str.length()==0)
+        {
+            return false;
+        }
+
+        for(int i =0; i<str.length(); i++)
+        {
+            
+
+            if(target==str.charAt(i))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+}
